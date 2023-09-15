e3c6b3e5c9592878fb25b5066716e8cce743e208
NobleCentaur <78610960+NobleCentaur@users.noreply.github.com>
2023-09-15 09:18:38 -0400

    ahhhhhh too many zeroes

--- GoVersions/mandelBrot.go
+++ GoVersions/mandelBrot.go
@@ -10,17 +10,25 @@ import (
 	"time"
 )
 
-var valueRange float64 = 2.5
-var imageSize int = 250
+// Image parameters
+// imageSize is the width and height in pixels
+// imageSharpness is the complexity of the math operations
+// ranges from 1 to the imageSize
+// lower values take longer to calculate
+// 5-10 is usually an acceptable balance between precision and speed
+var imageSize int = 1000
 var imageSharpness int = 1
+
+// other important values
+var valueRange float64 = 2.5
 var maxIteration int = imageSize / imageSharpness
 var startingX float64 = -2
 var startingY float64 = 1.25
 var complexNum complex128
 var step float64 = valueRange / (float64(imageSize) - 1)
 
+// simple, unoptimized escape time algorithm
 func escapeTimeAlgorithm(c complex128) uint8 {
-	//uses the escape time algorithm
 	var z complex128
 	var n uint8
 	for i := 0; i < maxIteration && cmplx.Abs(z) < 2; i++ {
@@ -46,6 +54,29 @@ func main() {
 			escapeTimeTable[j][k] = escapeTimeAlgorithm(complexNum)
 		}
 	}
+	//gradient starting and ending rgb values
+	var startingRed float32 = 0
+	var startingGreen float32 = 0
+	var startingBlue float32 = 0
+
+	var endingRed float32 = 0
+	var endingGreen float32 = 255
+	var endingBlue float32 = 0
+
+	var clrRed float32
+	var clrBlue float32
+	var clrGreen float32
+
+	escapeHistogram := make([]uint8, maxIteration)
+	var varTemp uint8
+	for j := 0; j < imageSize; j++ {
+		for k := 0; k < imageSize; k++ {
+			varTemp = escapeTimeTable[j][k]
+			escapeHistogram[varTemp-1]++
+		}
+	}
+	fmt.Println(escapeHistogram)
+
 	//interprets the escape time table to an image
 	img := image.NewRGBA(image.Rect(0, 0, imageSize, imageSize))
 	for j := 0; j < imageSize; j++ {
@@ -54,11 +85,14 @@ func main() {
 			if escapeTime == uint8(maxIteration) {
 				img.Set(j, k, color.RGBA{0, 0, 0, 255})
 			} else {
-				clr := (5 + (uint8((5-255)/maxIteration))*escapeTime)
-				img.Set(j, k, color.RGBA{clr, 0, 0, 255})
+				clrRed = (startingRed + ((endingRed-startingRed)/float32(maxIteration))*float32(escapeTime))
+				clrBlue = (startingBlue + ((endingBlue-startingBlue)/float32(maxIteration))*float32(escapeTime))
+				clrGreen = (startingGreen + ((endingGreen-startingGreen)/float32(maxIteration))*float32(escapeTime))
+				img.Set(j, k, color.RGBA{uint8(clrRed), uint8(clrBlue), uint8(clrGreen), 255})
 			}
 		}
 	}
+
 	//renders the image
 	f, _ := os.OpenFile("output.png", os.O_WRONLY|os.O_CREATE, 0600)
 	defer f.Close()
