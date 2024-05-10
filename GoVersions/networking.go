d6c6db0f64e758d4fcb2b38d69428351e6e27bc4
NobleCentaur <78610960+NobleCentaur@users.noreply.github.com>
2024-05-10 15:27:09 -0400

    added waitingRead()
    takes a conn object and a timeout in seconds. Returns a value and an error message.

--- GoVersions/networking.go
+++ GoVersions/networking.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"net"
+	"time"
 )
 
 var key = []byte{124, 44, 32, 124, 33, 44, 32, 124, 124, 44, 32, 124, 95, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 var buf = []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-var workerList []string
+var workerList []net.Conn
 var input string
+var memCapacity int
 
 var startingX_local float64
 var startingY_local float64
@@ -25,7 +27,7 @@ func addWorker() {
 	_, err = conn.Read(buf)
 	errHandler(err)
 	if Float64frombytes(buf) == Float64frombytes(key) {
-		workerList = append(workerList, input)
+		workerList = append(workerList, conn)
 		fmt.Println("Connection successful, [" + input + "] added to worker list")
 	} else {
 		networkMain()
@@ -36,6 +38,42 @@ func errHandler(e error) {
 	if e != nil {
 		fmt.Println(e)
 		return
+	}
+}
+
+// only used for waitingRead. Allows waitingRead to run the below in a goroutine.
+func readContinuous(ch chan []byte, eCh chan error, connection net.Conn) {
+	for {
+		data := make([]byte, 64)
+		_, err := connection.Read(data)
+		if err != nil {
+			eCh <- err
+			return
+		}
+		if Float64frombytes(data) != 0 {
+			ch <- data
+			break
+		}
+	}
+}
+
+// waits until conn.read actually includes data then sends it to ch channel
+func waitingRead(connection net.Conn, timeout int) (float64, error) {
+	ch := make(chan []byte)
+	eCh := make(chan error)
+
+	go readContinuous(ch, eCh, connection)
+
+	ticker := time.Tick(time.Second * time.Duration(timeout))
+	for {
+		select {
+		case data := <-ch:
+			return Float64frombytes(data), nil
+		case err := <-eCh:
+			fmt.Println(err)
+		case <-ticker:
+			return 0, net.ErrClosed
+		}
 	}
 }
 
@@ -63,7 +101,7 @@ func networkMain() {
 }
 
 func networkWorker() {
-	//listen for connectiosn
+	//listen for connections
 	fmt.Println("listening for connection from main")
 	ln, err := net.Listen("tcp", ":8080")
 	errHandler(err)
@@ -77,49 +115,20 @@ func networkWorker() {
 		conn.Write(key)
 	}
 
-	conn.Close()
+	fmt.Println(waitingRead(conn, 1800))
 
-	for stop := false; !stop; {
-		ln, err = net.Listen("tcp", ":8080")
-		errHandler(err)
+	time.Sleep(60 * time.Second)
 
-		for i := 0; i < 1000; i++ {
-			fmt.Print("8=======D, ")
-		}
+	//imageSize_local
+	//startingX_local
+	//startingY_local
+	//step_local
+	fmt.Println(imageSize_local)
+	fmt.Print(startingX_local)
+	fmt.Println(startingY_local)
+	fmt.Println(step_local)
+	for stopNested := false; !stopNested; {
 
-		conn, err := ln.Accept()
-
-		errHandler(err)
-		defer conn.Close()
-
-		_, err = conn.Read(buf)
-		errHandler(err)
-		imageSize_local = Float64frombytes(buf)
-		conn.Write([]byte{1})
-
-		_, err = conn.Read(buf)
-		errHandler(err)
-		startingX_local = Float64frombytes(buf)
-		conn.Write([]byte{1})
-
-		_, err = conn.Read(buf)
-		errHandler(err)
-		startingY_local = Float64frombytes(buf)
-		conn.Write([]byte{1})
-
-		_, err = conn.Read(buf)
-		errHandler(err)
-		step_local = Float64frombytes(buf) / (float64(imageSize_local) - 1)
-		// receive step
-
-		fmt.Println(imageSize_local)
-		fmt.Print(startingX_local)
-		fmt.Println(startingY_local)
-		fmt.Println(step_local)
-
-		for stopNested := false; !stopNested; {
-
-		}
 	}
 }
 
@@ -127,17 +136,30 @@ func networkRender() {
 	fmt.Println("")
 	fmt.Print("image Size (min 256): ")
 	fmt.Scan(&imageSize)
+	if imageSize%2 != 0 {
+		imageSize += 1
+	}
+	areYouSure := "n"
+	for areYouSure != "y" {
+		fmt.Print("Allowed memory usage (MB): ")
+		fmt.Scan(&memCapacity)
+		fmt.Println("Using " + fmt.Sprint(memCapacity) + " MB of memory")
+		fmt.Println("[ATTENTION] Using more ram than you device has will crash it.")
+		fmt.Print("Are you sure (y/n)")
+		fmt.Scan(&areYouSure)
+	}
+
 	maxIteration = imageSize / imageSharpness
 	step = valueRange / (float64(imageSize) - 1)
 
 	for i := 0; i < len(workerList); i++ {
 		go workerHandler(workerList[i])
-		fmt.Println("Process spawned for [" + workerList[i] + "]")
+		fmt.Println("Process spawned for Worker" + string(i))
 	}
 
 }
 
-func workerHandler(worker string) {
-	// conn, err := net.Dial("tcp", worker+"8080")
+func workerHandler(worker net.Conn) {
+	worker.Write(Float64bytes(float64(imageSize)))
 
 }
